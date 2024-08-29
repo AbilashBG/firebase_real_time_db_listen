3abecf49e46f61224451c0e6489f071cdf53967f
AbilashBG <86579609+AbilashBG@users.noreply.github.com>
2024-08-29 21:28:32 +0530

    iitial_commit

---
+++ lib/utils/api_end_points.dart
@@ -0,0 +1,7 @@
+
+// https://625f-103-182-69-2.ngrok-free.app/api/stored-texts
+
+String baseUrl = "625f-103-182-69-2.ngrok-free.app";
+String subUrl = "/api/";
+String textUrl = "${subUrl}stored-texts";
+
